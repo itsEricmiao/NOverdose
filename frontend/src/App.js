3f7f77b2e6dd0e73948f742b60f4f868e70f5ed4
Eric Miao <hongyum@smu.edu>
2020-03-31 18:29:43 -0500

    reinstalled react-dom module

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,72 +1,13 @@
 import React from 'react';
 import './App.css';
-import axios from 'axios';
+import Login from './containers/login.js'
 
-class App extends React.Component {
-
-  constructor(props){
-    super(props);
-    this.state = {
-      number: "",
-      values: []
-    };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange(e) {
-    this.setState({number: e.target.value})
-  }
-
-  handleSubmit(e){
-    e.preventDefault();
-    let prod = this.state.number * this.state.number;
-    axios.post('http://localhost:8000/multplynumber', {product: prod}).then(res => {
-      console.log(res);
-      this.fetchVals();
-    });
-    this.setState({number: ""});
-  }
-
-  initSetup = () => {
-    axios.post('http://localhost:8000/setupdb').then(res => {
-      console.log(res);
-      this.fetchVals();
-    });
-  }
-
-  componentDidMount(){
-    this.fetchVals();
-  }
-
-  fetchVals = () => {
-    axios.get('http://localhost:8000/values').then(
-      res => {
-        const values = res.data;
-        console.log(values.data);
-        this.setState({ values: values.data });
-    });
-  }
-
-
-  render(){
-    return (
-      <div className="App">
-        <header className="App-header">
-        <button onClick={this.initSetup}> Initialize DB </button>
-          <form onSubmit={this.handleSubmit}>
-            <input type="text" value={this.state.number} onChange={this.handleChange}/>
-            <br/>
-            <input type="submit" value="Submit" />
-          </form>
-          <ul>
-            { this.state.values.map((value, i) => <li key={i}>{value.value}</li>) }
-          </ul>
-        </header>
-      </div>
-    );
-  }
-
+function App() {
+  return (
+    <>
+        <Login />
+    </>
+  );
 }
 
 export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import * as serviceWorker from './serviceWorker';
+import Login from './containers/login.js';
+import Register from'./containers/register.js';
+import HomePage from './containers/homePage.js';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const routing = (
+  <Router>
+    <div>
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
+        <Route path="/homePage" component={HomePage} />
+      </Switch>
+    </div>
+  </Router>
+)
+ReactDOM.render(routing, document.getElementById('root'))
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
