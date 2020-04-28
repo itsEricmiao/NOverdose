97fda7ec2f739827ef098eaeda6533f3bd98e993
Corey23 <corey.ellis23@icloud.com>
2020-04-28 17:14:56 -0500

    symptom sample data insert statements
    only has insert statement no creation or alter

---
+++ symptomstable.sql
@@ -0,0 +1,11 @@
+
+insert into symptoms values 
+(NULL, "Bad Breath"), (NULL, "Constipation"), (NULL, "Coughing"), (NULL, "Dandruff"),
+ (NULL, "Dehydration"), (NULL, "Diarrhea"), (NULL, "Dizziness"), (NULL, "Eye Irratation"),
+ (NULL, "Fever"), (NULL, "Hair Loss"), (NULL, "Itching"), (NULL, "Limping"), 
+ (NULL, "Lethargy"), (NULL, "Lumps"), (NULL, "Nose Bleed"), (NULL, "Runny Nose"), 
+ (NULL, "Seizurea"), (NULL, "Trembling"), (NULL, "Vomiting"), (NULL, "Weight Gain");
+
+
+ 
+ 
